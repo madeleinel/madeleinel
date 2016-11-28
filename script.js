7ff4d6b8e5e283b751b2f140ccbc3b7ee6b9579c
Madeleine Linder <madeleinelinder@hotmail.com>
2016-11-28 23:53:56 +0000

    Commit 2
    Checking the look of the newly added Timeline section & minor layout
    design changes

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function expandLeft() {
-  document.getElementById("leftSidenav").style.left = "0";
-  document.getElementById("middleSidenav").style.left = "80%";
-  document.getElementById("rightSidenav").style.left = "90%";
-  document.getElementById("titleLeft").style.left = "40%";
-  document.getElementById("titleMiddle").style.left = "0";
-  document.getElementById("titleRight").style.left = "0";
+    document.getElementById("leftSidenav").style.left = "0";
+    document.getElementById("middleSidenav").style.left = "80%";
+    document.getElementById("rightSidenav").style.left = "90%";
+    document.getElementById("titleLeft").style.left = "40%";
+    document.getElementById("titleMiddle").style.left = "0";
+    document.getElementById("titleRight").style.left = "0";
 }
 
 function expandMiddle() {
-  document.getElementById("leftSidenav").style.left = "0";
-  document.getElementById("middleSidenav").style.left = "10%";
-  document.getElementById("rightSidenav").style.left = "90%";
-  document.getElementById("titleLeft").style.left = "";
-  document.getElementById("titleMiddle").style.left = "40%";
-  document.getElementById("titleRight").style.left = "0";
+    document.getElementById("leftSidenav").style.left = "0";
+    document.getElementById("middleSidenav").style.left = "10%";
+    document.getElementById("rightSidenav").style.left = "90%";
+    document.getElementById("titleLeft").style.left = "0";
+    document.getElementById("titleMiddle").style.left = "40%";
+    document.getElementById("titleRight").style.left = "0";
 }
 
 function expandRight() {
-  document.getElementById("leftSidenav").style.left = "0";
-  document.getElementById("middleSidenav").style.left = "10%";
-  document.getElementById("rightSidenav").style.left = "20%";
-  document.getElementById("titleLeft").style.left = "0";
-  document.getElementById("titleMiddle").style.left = "0";
-  document.getElementById("titleRight").style.left = "40%";
+    document.getElementById("leftSidenav").style.left = "0";
+    document.getElementById("middleSidenav").style.left = "10%";
+    document.getElementById("rightSidenav").style.left = "20%";
+    document.getElementById("titleLeft").style.left = "0";
+    document.getElementById("titleMiddle").style.left = "0";
+    document.getElementById("titleRight").style.left = "40%";
 }
