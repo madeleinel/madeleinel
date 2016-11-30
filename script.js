13c2701cf7f209450676d91bb203023b5786ba83
madeleinel <madeleinelinder@hotmail.com>
2016-11-30 16:56:27 +0000

    Revert to old version
    Revert to commit 17 - before favicons & attempting to fix overflow
    issues

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function expandLeft() {
-document.getElementById("sectionLeft").style.left = "0";
-document.getElementById("sectionMiddle").style.left = "80%";
-document.getElementById("sectionRight").style.left = "90%";
-document.getElementById("titleLeft").style.left = "40%";
-document.getElementById("titleMiddle").style.left = "0";
-document.getElementById("titleRight").style.left = "0";
+  document.getElementById("sectionLeft").style.left = "0";
+  document.getElementById("sectionMiddle").style.left = "80%";
+  document.getElementById("sectionRight").style.left = "90%";
+  document.getElementById("titleLeft").style.left = "40%";
+  document.getElementById("titleMiddle").style.left = "0";
+  document.getElementById("titleRight").style.left = "0";
 }
 
 function expandMiddle() {
-document.getElementById("sectionLeft").style.left = "0";
-document.getElementById("sectionMiddle").style.left = "10%";
-document.getElementById("sectionRight").style.left = "90%";
-document.getElementById("titleLeft").style.left = "0";
-document.getElementById("titleMiddle").style.left = "40%";
-document.getElementById("titleRight").style.left = "0";
+  document.getElementById("sectionLeft").style.left = "0";
+  document.getElementById("sectionMiddle").style.left = "10%";
+  document.getElementById("sectionRight").style.left = "90%";
+  document.getElementById("titleLeft").style.left = "0";
+  document.getElementById("titleMiddle").style.left = "40%";
+  document.getElementById("titleRight").style.left = "0";
 }
 
 function expandRight() {
-document.getElementById("sectionLeft").style.left = "0";
-document.getElementById("sectionMiddle").style.left = "10%";
-document.getElementById("sectionRight").style.left = "20%";
-document.getElementById("titleLeft").style.left = "0";
-document.getElementById("titleMiddle").style.left = "0";
-document.getElementById("titleRight").style.left = "40%";
+  document.getElementById("sectionLeft").style.left = "0";
+  document.getElementById("sectionMiddle").style.left = "10%";
+  document.getElementById("sectionRight").style.left = "20%";
+  document.getElementById("titleLeft").style.left = "0";
+  document.getElementById("titleMiddle").style.left = "0";
+  document.getElementById("titleRight").style.left = "40%";
 }
